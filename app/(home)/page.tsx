c6e2def82358d523085845d79cd33e066c509efa
rishika-on-git <aggarwalrishika135@gmail.com>
2024-08-03 21:42:16 +0530

    finalll-chng

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,9 @@
 import { GridBackgroundDemo } from "./components/GridBackgroundDemo";
 import { BackgroundBeamsDemo } from "./components/BackgroundBeamsDemo";
-import { StickyScrollRevealDemo } from "./components/StickyScrollRevealDemo";
+
 import { FloatingNavDemo } from "./components/FloatingNavDemo";
 import { ExpandableCardDemo } from "./components/ExpandableCardDemo";
-import { LinkPreviewDemo } from "./components/LinkPreviewDemo";
+
 
 export default function Home() {
   return (
